f77b5d693ca1e8879002b95b116f2a5ec7bb0753
Abhijeet Gupta <102848425+auraticabhi@users.noreply.github.com>
2023-10-07 12:40:55 +0530

    Update CodeBlocks.jsx

--- StudyNotion/src/components/core/HomePage/CodeBlocks.jsx
+++ StudyNotion/src/components/core/HomePage/CodeBlocks.jsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from 'react-type-animation'
 
 const CodeBlocks = ({position, heading, subheading, ctabtn1, ctabtn2, codeblock, backgroundGradient, codeColor }) => {
   return (
-    <div className={`flex ${position} my-20 justify-between lg:gap-10 gap-10`}>
+    <div className={`flex ${position} my-20 justify-between flex-col lg:gap-10 gap-10`}>
     
     <div className="w-[100%] lg:w-[50%] flex flex-col gap-8">
     {heading}
