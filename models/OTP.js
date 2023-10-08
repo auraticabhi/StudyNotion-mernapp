7ee49f2b50c07b052bf2079c64d805bc2f6edd9e
Abhijeet Gupta <102848425+auraticabhi@users.noreply.github.com>
2023-10-08 13:58:09 +0530

    Update OTP.js

--- models/OTP.js
+++ models/OTP.js
@@ -14,7 +14,7 @@ const OTPSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now(),
-        expireAt: { type: Date, expires: 600 },
+        expireAt: { type: Date, expires: 500 },
     }
 });
 
