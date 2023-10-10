61bd3e846ebe0251400f7d8be23e054b6156ab43
Abhijeet Gupta <abhijeetgupta989@gmail.com>
2023-10-10 22:27:22 +0530

    Update mailSender.js

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -11,7 +11,7 @@ const mailSender = async(email, title, body) => {
         })
 
         let info = await transporter.sendMail({
-            from: 'StudyNotion',
+            from: `"StudyNotion" <${process.env.MAIL_USER}>`,
             to: `${email}`,
             subject: `${title}`,
             html: `${body}`
